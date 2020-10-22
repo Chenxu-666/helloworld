fab3f60117266dc98bef99fcb84d6ff395b1159f
Mr_Chen <60974499+Chenxu-666@users.noreply.github.com>
2020-10-22 22:53:45 +0800

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,6 @@
 public class Hello(){
      public static void main(String[]args){
         System.out.println("IDEA整合Git后，创建的第一个文件，并提交到远程库！")
+        System.out.println("在远程库进行修改，然后在本地update。得到更新后的结果")
         }
         }
